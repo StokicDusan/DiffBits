16806b9f79ba5a4d98df3ae9186f26ad1bd5b827
StokicDusan <stokicdusan@gmail.com>
2021-11-16 14:18:32 +0100

    code punch up

--- diffbits.py
+++ diffbits.py
@@ -1,10 +1,11 @@
 from colorama import Fore, Back, Style, init
-import sys, os
+from sys import argv
+from os import path
 init()
 
 def main(filePath):
-    file1 = open(filePath,'r')
-    linije = file1.readlines()
+    with open(filePath,'r') as f:
+        linije = f.readlines()
     length = 0
     for i in range(0,len(linije)):
         linije[i]=linije[i].rstrip()
@@ -15,25 +16,25 @@ def main(filePath):
             diff_array.append("s") if all(str(item[i])=='0' for item in linije) or all(str(item[i])=='1' for item in linije) else diff_array.append("d")
         except IndexError:
             diff_array.append("l")
-    for i in range(0,len(linije)):
-        output(linije[i],length,diff_array)
+    for item in linije:
+        output(item,diff_array)
 
-def output(string,length,diff_array):
+def output(string,diff_array):
     try:
-        for i in range(0,length):
-            if diff_array[i] == "d":
-                print(Fore.WHITE + string[i], end="")
-            elif diff_array[i] == "s":
-                print(Fore.GREEN + string[i], end="")
-            elif diff_array[i] == "l":
-                print(Fore.YELLOW + string[i], end="")
+        for diff,string in zip(diff_array, string):
+            if diff == "d":
+                print(Fore.WHITE + string, end="")
+            elif diff == "s":
+                print(Fore.GREEN + string, end="")
+            elif diff == "l":
+                print(Fore.YELLOW + string, end="")
     except IndexError:
         pass
     print()
 
 if __name__ == "__main__":
-    if(sys.argv[1:]):
-        main(os.path.basename(sys.argv[1]))
+    if(argv[1:]):
+        main(path.basename(argv[1]))
     else:
         print("no file passed \n")
         pass
